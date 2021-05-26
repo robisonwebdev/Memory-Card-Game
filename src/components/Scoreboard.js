13c7f274d520015dee9cd5e4ba07e9ee5efd4c6a
David Robison <david@robisonweb.dev>
2021-05-26 15:14:32 -0600

    Created 2 divs with ID's of gameTitle and scores. Inside gameTitle, put h1 with title. Inside score, put 2 p elements with ID's of gameScore and highScore. gameScore and highScore both accept props.

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,6 +1,14 @@
-const Scoreboard = () => {
+const Scoreboard = (props) => {
     return (
-        <div id="Scoreboard">Scoreboard</div>
+        <div id="scoreboard">
+            <div id='gameTitle'>
+                <h1>Memory Game</h1>
+            </div>
+            <div id='scores'>
+                <p id='gameScore'>{props.gameScore}</p>
+                <p id='highScore'>{props.highScore}</p>
+            </div>
+        </div>
     );
 }
 
