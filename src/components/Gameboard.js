fadfa800359eb3c63585316752d492e3419fe359
David Robison <david@robisonweb.dev>
2021-05-27 10:43:16 -0600

    Imported and called Card

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,9 +1,10 @@
 import '../styles/Gameboard.css';
+import Card from './Card';
 
 const Gameboard = () => {
     return (
         <div id='Gameboard'>
-
+            <Card />
         </div>
     );
 }
