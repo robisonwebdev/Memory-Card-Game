8afdd6cc280b68d4ae633318a086b0c29515735b
David Robison <david@robisonweb.dev>
2021-05-27 11:05:09 -0600

    Added temp img src

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,9 @@ import '../styles/Card.css';
 const Card = () => {
     return (
         <div className='card'>
-            <div className='cardImage'>Test Image</div>
+            <div className='cardImage'>
+                <img src='https://images.unsplash.com/photo-1465768210703-d6986e3c1728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' />
+            </div>
             <div className='cardText'>Test Text</div>
         </div>
     );
