4f9e10c301cbad4be994b17b77ab6619b2818213
David Robison <david@robisonweb.dev>
2021-05-28 10:56:34 -0600

    Enter information for each card

---
+++ src/components/CardData.js
@@ -0,0 +1,65 @@
+import Jack from '../images/jackOneill.jpg';
+import Carter from '../images/Cartersamantha.jpg';
+import Daniel from '../images/Jacksondaniel.jpg';
+import Tealc from '../images/Tealc.jpg';
+import Cameron from '../images/Mitchellcameron.jpg';
+import Vala from '../images/Maldoranvala.jpg';
+import Jonas from '../images/Quinnjonas.jpg';
+import George from '../images/Hammondgeorge.jpg';
+import Hank from '../images/Landryhank.jpg';
+import Janet from '../images/Fraiserjanet.jpg';
+
+const CardData = [
+    {
+        id: 1,
+        source: Jack,
+        text: "Jack O'Neill"
+    },
+    {
+        id: 2,
+        source: Carter,
+        text: "Samantha Carter"
+    },
+    {
+        id: 3,
+        source: Daniel,
+        text: "Daniel Jackson"
+    },
+    {
+        id: 4,
+        source: Tealc,
+        text: "Teal'c"
+    },
+    {
+        id: 5,
+        source: Cameron,
+        text: "Cameron Mitchell"
+    },
+    {
+        id: 6,
+        source: Vala,
+        text: "Vala Mal Doran"
+    },
+    {
+        id: 7,
+        source: Jonas,
+        text: "Jonas Quinn"
+    },
+    {
+        id: 8,
+        source: George,
+        text: "George Hammond"
+    },
+    {
+        id: 9,
+        source: Hank,
+        text: "Hank Landry"
+    },
+    {
+        id: 10,
+        source: Janet,
+        text: "Janet Fraiser"
+    }
+]
+
+export default CardData;
