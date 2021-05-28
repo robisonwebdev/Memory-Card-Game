7f823f3c29ab56e6fde42121dd5a4a44e5fcc1b5
David Robison <david@robisonweb.dev>
2021-05-28 10:27:15 -0600

    Img and text now accept props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import '../styles/Card.css';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='card'>
             <div className='cardImage'>
-                <img src='https://images.unsplash.com/photo-1465768210703-d6986e3c1728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' />
+                <img src={props.source} />
             </div>
-            <div className='cardText'>Test Text</div>
+            <div className='cardText'>{props.text}</div>
         </div>
     );
 }
