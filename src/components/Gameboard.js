0222c738ac1d70b89cc88f671d1f90d8a8f5e3ed
David Robison <david@robisonweb.dev>
2021-05-28 10:59:16 -0600

    Added Card's to test source and text

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,10 +1,20 @@
 import '../styles/Gameboard.css';
 import Card from './Card';
+import CardData from './CardData';
 
 const Gameboard = () => {
     return (
         <div id='Gameboard'>
-            <Card />
+            <Card source={CardData[0].source} text={CardData[0].text} />
+            <Card source={CardData[1].source} text={CardData[1].text} />
+            <Card source={CardData[2].source} text={CardData[2].text} />
+            <Card source={CardData[3].source} text={CardData[3].text} />
+            <Card source={CardData[4].source} text={CardData[4].text} />
+            <Card source={CardData[5].source} text={CardData[5].text} />
+            <Card source={CardData[6].source} text={CardData[6].text} />
+            <Card source={CardData[7].source} text={CardData[7].text} />
+            <Card source={CardData[8].source} text={CardData[8].text} />
+            <Card source={CardData[9].source} text={CardData[9].text} />
         </div>
     );
 }
