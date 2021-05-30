3c33bf4c31bb0d1bab65076248d4ce7310ec51f0
David Robison <david@robisonweb.dev>
2021-05-30 11:59:36 -0600

    Removed committed code

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -10,16 +10,6 @@ const Gameboard = () => {
                    return <Card source={card.source} text={card.text} />
                 })
             }
-            {/* <Card source={CardData[0].source} text={CardData[0].text} />
-            <Card source={CardData[1].source} text={CardData[1].text} />
-            <Card source={CardData[2].source} text={CardData[2].text} />
-            <Card source={CardData[3].source} text={CardData[3].text} />
-            <Card source={CardData[4].source} text={CardData[4].text} />
-            <Card source={CardData[5].source} text={CardData[5].text} />
-            <Card source={CardData[6].source} text={CardData[6].text} />
-            <Card source={CardData[7].source} text={CardData[7].text} />
-            <Card source={CardData[8].source} text={CardData[8].text} />
-            <Card source={CardData[9].source} text={CardData[9].text} /> */}
         </div>
     );
 }
