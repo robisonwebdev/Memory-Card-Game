b2c118c96e917e9dff910c5d1a73fe2248166dbc
David Robison <david@robisonweb.dev>
2021-05-30 11:58:22 -0600

    Mapped CardData and returned <Card> component for each map, with props

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -5,7 +5,12 @@ import CardData from './CardData';
 const Gameboard = () => {
     return (
         <div id='Gameboard'>
-            <Card source={CardData[0].source} text={CardData[0].text} />
+            {
+                CardData.map((card) => {
+                   return <Card source={card.source} text={card.text} />
+                })
+            }
+            {/* <Card source={CardData[0].source} text={CardData[0].text} />
             <Card source={CardData[1].source} text={CardData[1].text} />
             <Card source={CardData[2].source} text={CardData[2].text} />
             <Card source={CardData[3].source} text={CardData[3].text} />
@@ -14,7 +19,7 @@ const Gameboard = () => {
             <Card source={CardData[6].source} text={CardData[6].text} />
             <Card source={CardData[7].source} text={CardData[7].text} />
             <Card source={CardData[8].source} text={CardData[8].text} />
-            <Card source={CardData[9].source} text={CardData[9].text} />
+            <Card source={CardData[9].source} text={CardData[9].text} /> */}
         </div>
     );
 }
