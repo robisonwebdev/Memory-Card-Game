4c47b6a00b7550dc776b5b49f4a769c43c943f9a
David Robison <david@robisonweb.dev>
2021-05-31 09:53:26 -0600

    Destructured props

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,14 +1,16 @@
 import '../styles/Scoreboard.css';
 
 const Scoreboard = (props) => {
+    const { gameScore, highScore } = props;
+
     return (
         <div id="scoreboard">
             <div id='gameTitle'>
                 <h1>Memory Game</h1>
             </div>
             <div id='scores'>
-                <p id='gameScore'>Score: {props.gameScore}</p>
-                <p id='highScore'>High Score: {props.highScore}</p>
+                <p id='gameScore'>Score: {gameScore}</p>
+                <p id='highScore'>High Score: {highScore}</p>
             </div>
         </div>
     );
