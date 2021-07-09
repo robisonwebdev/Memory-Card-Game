b19ae9bbe8bf71bf06e1427a6408be1e92a24ac2
David Robison <david@robisonweb.dev>
2021-07-09 12:37:41 -0600

    Destructed props

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -2,9 +2,9 @@ import '../styles/Gameboard.css';
 import Card from './Card';
 import CardData from './CardData';
 
-const Gameboard = (props) => {
+const Gameboard = ({ click }) => {
     let randomCard = CardData.sort(() => Math.random() - 0.5);
-    const mappedCardData = randomCard.map((card) => <Card {...card} key={card.id} click={props.click} />);
+    const mappedCardData = randomCard.map((card) => <Card {...card} key={card.id} click={click} />);
 
     return (
         <div id='Gameboard'>
